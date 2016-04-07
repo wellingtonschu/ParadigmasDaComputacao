9453af8f456561721fe99e88022fb9ee77da1e42
Wellington Schütz <wellington.schu@live.com>
2016-04-07 17:22:20 -0300

    Implementação em Java

---
+++ src/src/Lista05/Ex05.java
@@ -0,0 +1,37 @@
+package Lista05;
+
+public class Ex05 {
+
+    public static void main(String[] args) {
+
+        int k = 6;
+        int j = 0;
+
+        switch (k) {
+
+            case 1 :case 2 :
+
+                j = 2 * k - 1;
+                break;
+
+            case 3 :case 5 :
+
+                j = 3 * k + 1;
+                break;
+
+            case 4 :
+
+                j = 4 * k + 1;
+                break;
+
+            case 6 :case 7 :case 8 :
+
+                j = k - 2;
+
+        }
+
+        System.out.println(j);
+
+    }
+
+}
